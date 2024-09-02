bc8f42c00b610b5e5b647ccb5599a45c571a7141
Heverton Luan Roieski <hevertonlr@gmail.com>
2024-09-01 22:00:34 -0300

    feat: Init Project

---
+++ src/main/java/com/lab365/app/fmtm3firsttask/FmtM3FirstTaskApplication.java
@@ -0,0 +1,13 @@
+package com.lab365.app.fmtm3firsttask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FmtM3FirstTaskApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FmtM3FirstTaskApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/lab365/app/fmtm3firsttask/FmtM3FirstTaskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lab365.app.fmtm3firsttask;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FmtM3FirstTaskApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
